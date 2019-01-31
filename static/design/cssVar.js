f9fd102b9399d9d6677977a1d05502ec834efb10
Artem <shariukov.artem@mail.ru>
2019-02-01 00:55:20 +0300

    next out / static build 1.02.19

---
+++ static/design/cssVar.js
@@ -0,0 +1,42 @@
+// css variables from styleguide
+
+const cssVar = {
+    font: {
+        lalezar: "'Lalezar', cursive"
+    },
+    color: {
+        blue: "#48C6EF",
+        blueDark: "#4099B8",
+        gray: "#787878",
+        grayDark: "#3A3A3A",
+        grayLight: "#B8B8B8",
+        grayLightest: "#E7E7E7",
+        whiteBackground: "#F9F9F9",
+        blackBackground: "#1B1B1B"
+    },
+    containerWidth: {
+        containerLg: "1200px",
+        containerSm: "100%"
+    },
+    fontSize: {
+        superBig: "90px",
+        headingBig: "24px",
+        heading: "20px",
+        paragraph: "16px",
+        small: "14px",
+        xSmall: "12px"
+    },
+    shadow: {
+        card: "0 0 6px 0 rgba(22,22,22,0.2)"
+    },
+    transition: {
+        base: ".1s"
+    },
+    zIndex: {
+        tooltips: "1",
+        overlay: "2",
+        modal: "3"
+    }
+};
+
+export default cssVar;
